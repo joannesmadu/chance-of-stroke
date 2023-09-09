fb5acaa64d2f189c50047ef88b26b5b315d1ef78
Joannes Madu <joannesmadu@gmail.com>
2023-09-09 23:48:09 +0100

    chance of stroke model 08/09 model/params

--- logic/model.py
+++ logic/model.py
@@ -3,68 +3,41 @@ import time
 
 from colorama import Fore, Style
 from typing import Tuple
+from keras import Model
 
 # Timing the TF import
-print(Fore.BLUE + "\nLoading TensorFlow..." + Style.RESET_ALL)
+print(Fore.BLUE + "\nLoading KNClassifier..." + Style.RESET_ALL)
 start = time.perf_counter()
 
 from sklearn.neighbors import KNeighborsClassifier
 
 end = time.perf_counter()
-print(f"\n✅ TensorFlow loaded ({round(end - start, 2)}s)")
+print(f"\n✅ KNClassifier loaded ({round(end - start, 2)}s)")
 
 
 
 def initialize_model(input_shape: tuple) -> Model:
 
+    model = KNeighborsClassifier(n_jobs=-1, n_neighbors=5)
 
     print("✅ Model initialized")
-
-    return model
-
-
-def compile_model(model: Model, learning_rate=0.0005) -> Model:
-    """
-    Compile the Neural Network
-    """
-    optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
-    model.compile(loss="mean_squared_error", optimizer=optimizer, metrics=["mae"])
-
-    print("✅ Model compiled")
 
     return model
 
 def train_model(
         model: Model,
         X: np.ndarray,
-        y: np.ndarray,
-        batch_size=256,
-        patience=2,
-        validation_data=None, # overrides validation_split
-        validation_split=0.3
-    ) -> Tuple[Model, dict]:
+        y: np.ndarray
+        ) -> Tuple[Model, dict]:
     """
     Fit the model and return a tuple (fitted_model, history)
     """
 
     print(Fore.BLUE + "\nTraining model..." + Style.RESET_ALL)
 
-    es = EarlyStopping(
-        monitor="val_loss",
-        patience=patience,
-        restore_best_weights=True,
-        verbose=1)
+    model.fit(X)
+    model.transform(X)
 
-    history = model.fit(
-        X,
-        y,
-        validation_data=validation_data,
-        validation_split=validation_split,
-        epochs=100,
-        batch_size=batch_size,
-        callbacks=[es],
-        verbose=0)
+    print(f"✅ Model trained on {len(X)} rows with {5} neighbors")
 
-    print(f"✅ Model trained on {len(X)} rows with min val MAE: {round(np.min(history.history['val_mae']), 2)}")
-
-    return model, history
+    return model
--- logic/params.py
+++ logic/params.py
@@ -1,24 +1,24 @@
 import os
 import numpy as np
 
-##################  VARIABLES  ##################
-DATA_SIZE = "500k"
-CHUNK_SIZE = 100000
-GCP_PROJECT = "wagon-bootcamp-392717"
-GCP_PROJECT_WAGON = "wagon-public-datasets"
-BQ_DATASET = "taxifare"
-BQ_REGION = "EU"
-MODEL_TARGET = "local"
 ##################  CONSTANTS  #####################
 LOCAL_DATA_PATH = os.path.join(os.path.expanduser('~'), "Downloads")
 LOCAL_REGISTRY_PATH =  os.path.join(os.path.expanduser('~'), "Downloads")
 
-COLUMN_NAMES_RAW = ['fare_amount','pickup_datetime', 'pickup_longitude', 'pickup_latitude', 'dropoff_longitude', 'dropoff_latitude', 'passenger_count']
+COLUMN_NAMES_RAW = ['id','gender','age', 'hypertension', 'heart_disease', 'ever_married', 'work_type', 'Residence_type','avg_glucose_level','bmi','smoking_status']
 
 DTYPES_RAW = {
+    "id":"int16",
+    "gender":"object",
     "age": "int16",
+    "hypertension":"int16",
+    "heart_disease":"int16",
+    "ever_married":"object",
+    "work_type":"object",
+    "Residence_type":"object",
     "bmi": "float32",
-    "avg_glucose_level": "float32"
+    "avg_glucose_level": "float32",
+    "smoking_status":"object"
 }
 
 DTYPES_PROCESSED = np.float32
@@ -26,20 +26,3 @@ DTYPES_PROCESSED = np.float32
 # "/Users/jmadu1/.lewagon/mlops/data/raw/query_2009-01-01_2015-01-01_1k.csv"
 # "/Users/jmadu1/.lewagon/mlops/data/processed/train_processed_1k.csv"
 # "/Users/jmadu1/Documents/healthcare-dataset-stroke-data.csv"
-
-"""
- #   Column             Non-Null Count  Dtype
----  ------             --------------  -----
- 0   id                 5110 non-null   int64
- 1   gender             5110 non-null   object
- 2   age                5110 non-null   float64
- 3   hypertension       5110 non-null   int64
- 4   heart_disease      5110 non-null   int64
- 5   ever_married       5110 non-null   object
- 6   work_type          5110 non-null   object
- 7   Residence_type     5110 non-null   object
- 8   avg_glucose_level  5110 non-null   float64
- 9   bmi                4909 non-null   float64
- 10  smoking_status     5110 non-null   object
- 11  stroke             5110 non-null   int64
-"""
